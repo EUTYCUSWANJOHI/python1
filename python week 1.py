aaf6e6de669e6516a7b2ec4f1337d4d0f51a66fc
EUTYCUSWANJOHI <wamaieutycus@gmail.com>
2024-02-23 16:58:43 +0300

    EUTYCUSWANJOHI

---
+++ python week 1.py
@@ -0,0 +1,5 @@
+name = input("what is your name? ")
+age = input("what is your age? ")
+location = input("where do you live? ")
+
+print(f"hello {name}, you are {age} years old you live in {location}.")
